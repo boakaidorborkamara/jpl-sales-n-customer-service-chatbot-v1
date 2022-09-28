433143e1406e7e088f70c92cfa88e69bc58ce8af
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-09-28 00:37:13 +0000

    modified env file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+require('dotenv').config();
+
 
 const PORT = process.env.PORT || 3000;
+console.log(process.env);
 
 const app = express();
 app.listen(PORT,()=>{

437ff3b1e385d650e0f1409c1fbc9ad1c24ee83f
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-10-09 13:58:18 +0000

    updated webhook get route

--- server.js
+++ server.js
@@ -14,29 +14,34 @@ app.get('/', (req, res)=>{
 })
 
 
-// an endpoint on that receives and processes Webhooks notifications, JSON objects
-app.post("/webhook", (req, res) => {
 
-    // get the request body 
-    let body = req.body;
-  
-    console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body, { depth: null });
-    
-  
-  // Send a 200 OK response if this is a page webhook
-  if (body.object === "page") {
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
+app.post('/webhook', (req, res) => {  
 
-    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-}); 
 
+});
 
 
 // Add support for GET requests to our webhook

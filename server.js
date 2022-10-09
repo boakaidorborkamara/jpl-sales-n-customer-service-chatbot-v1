6bd45f40f0f28c477ff8efe9acedce7e1fc8af8b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-10-09 23:19:39 +0000

    fixed bug faced with verify token

--- server.js
+++ server.js
@@ -62,9 +62,12 @@ app.get("/messaging-webhook", (req, res) => {
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
+    console.log(mode);
+    console.log(token);
+    console.log(challenge);
 
     const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    const VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
   
     // Check if a token and mode is in the query string of the request
     if (mode && token) {
@@ -75,6 +78,7 @@ app.get("/messaging-webhook", (req, res) => {
         res.status(200).send(challenge);
       } else {
         // Respond with '403 Forbidden' if verify tokens do not match
+        console.log("forbidden request");
         res.sendStatus(403);
       }
     }

747bcfef42f36a58602660dede724711eb62aceb
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-10-21 02:01:00 +0000

    added a function that tell if a message is read

--- services/homePageService.js
+++ services/homePageService.js
@@ -105,3 +105,25 @@ let sendTypingOn = async (sender_psid)=>{
 
     // let content = await response;
 }
+
+//notify user when message is being read
+let markMessageRead = (sender_psid)=>{
+    let url = `https://graph.facebook.com/v6.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "sender_action": "mark_seen"
+    };
+
+    const response = await fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':"application/json"
+        },
+        body:JSON.stringify(request_body)
+    });
+}
+
+

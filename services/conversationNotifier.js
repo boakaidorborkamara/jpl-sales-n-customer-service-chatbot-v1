cc4d4cbe2074624ec5aa338496aac38829f3c728
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-10-23 22:57:41 +0000

    enabled chatbot to mark user message as read and notfy user when it is typing

--- services/conversationNotifier.js
+++ services/conversationNotifier.js
@@ -1,35 +1,54 @@
 require("dotenv").config();
-const fetch = require("node-fetch");
+const request  = require("request");
 
 //notify user when the bot is typing
 const sendTypingOn = async function (sender_psid){
 
-    let url = `https://graph.facebook.com/v6.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
 
-    let request_body = {
+     // data to be sent to facebook server using the fb graph api
+     let request_body = {
         "recipient": {
             "id": sender_psid
         },
         "sender_action": "typing_on"
     };
 
-    const response = await fetch(url,{
-        method:'POST',
+    //facebook graph api
+    let url = `https://graph.facebook.com/v6.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
+
+
+    let options = {
+        url:url,
         headers:{
             'Content-Type':"application/json"
         },
-        body:JSON.stringify(request_body)
-    });
+        method:'POST',
+        json:request_body
+    }
 
-    let content = await response.json();
+    try{
+        //make http request
+        request(options,(err,res,body)=>{
+            if(err){
+                console.log(err);
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
 
-    return content;
+   
+
 }
 
 //notify user when message is being read
 let markMessageRead = async (sender_psid)=>{
+
+    //facebook graph api
     let url = `https://graph.facebook.com/v6.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
 
+    // data to be sent to facebook server using the fb graph api
     let request_body = {
         "recipient": {
             "id": sender_psid
@@ -37,13 +56,28 @@ let markMessageRead = async (sender_psid)=>{
         "sender_action": "mark_seen"
     };
 
-    const response = await fetch(url,{
-        method:'POST',
-        headers:{
-            'Content-Type':"application/json"
-        },
-        body:JSON.stringify(request_body)
-    });
+    try{
+
+        let options = {
+            url:url,
+            headers:{
+                'Content-Type':"application/json"
+            },
+            method:'POST',
+            json:request_body
+        }
+
+        //make http request
+        request(options,(err,res,body)=>{
+            if(err){
+                console.log(err);
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+
 }
 
 
